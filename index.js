d4b66e27bd0dd95382186b091d8bed6c47d2d60d
Ian Sinnott <ian@iansinnott.com>
2016-06-23 16:41:28 -0700

    Add initial webpack config

---
+++ index.js
@@ -0,0 +1,5 @@
+// index.js
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(1, 2));
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  entry: './index.js',
+
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+  },
+
+};
